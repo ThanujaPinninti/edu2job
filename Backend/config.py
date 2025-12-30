c123cdd48ffc94c8fe26b7a72a8499664ea718dd
Thanuja <156277448+ThanujaPinninti@users.noreply.github.com>
2025-12-30 13:10:01 +0530

    Backend
    EDU2JOB

---
+++ Backend/config.py
@@ -0,0 +1,14 @@
+# Config.py
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "Thanuja@2002",
+    "database": "edu"
+}
+
+SECRET_KEY = "my-super-secure-jwt-key-12345"
+GOOGLE_CLIENT_ID = "141192445026-pkvqu868edv3hc7rgqp69e5j3d5m5ht7.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+UPLOAD_FOLDER = r"E:\EDU2JOB\backend\uploads"
